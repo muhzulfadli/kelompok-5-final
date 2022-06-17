ae5258241508a9b2d94e790f541ec844dba85fb8
Gemasss <58686523+Gemafauzi@users.noreply.github.com>
2022-06-17 20:35:12 +0700

    Product Preview & Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import Product from './Pages/Product/ProductSeller'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layouts from './Layouts/Layouts'
+import ProductPreview from './Pages/Product/ProductPreview'
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div className="font-main">
+        <Routes>
+          <Route path="/" element={<Layouts />}>
+            <Route path="/ProductSeller" element={<Product />} />
+            <Route path="/ProductPreview" element={<ProductPreview />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+
+
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ module.exports = {
     extend: {
       fontFamily: {
         main: ['"Poppins"', 'sans-serif'],
-      }
+      },
+      colors: {
+        purple1: '#E2D4F0',
+        purple2: '#D0B7E6',
+        purple3: '#A06ECE',
+        purple4: '#7126B5',
+        purple5: '#4B1979',
+      },
+      boxShadow: {
+        'auto': '0px 0px 4px rgba(0, 0, 0, 0.15)'
+      },
+      fontSize: {
+        'tiny': '.625rem',
+      },
     },
   },
   plugins: [],
