a5f0b8e3d820e52795798c64edacb6ba444f6e00
Zulfadli <muh.fadli987@gmail.com>
2022-07-06 22:41:33 +0800

    Homepage integrated fakeapi

--- src/Components/Product/ProductCard.js
+++ src/Components/Product/ProductCard.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Zoom from 'react-medium-image-zoom'
 
-const ProductCard = () => {
+const ProductCard = (props) => {
     return (
         <div className="h-60 rounded-2xl border border-slate-300 p-3 shadow-md">
             <div className="flex h-1/2 items-center justify-center rounded-md ">
-             <img src="/images/JamtanganBiru.png" alt="" />
+                <div className="flex items-center w-1/3 h-1/3 ">
+                    <Zoom>
+                        <img src={props.product.image} alt="" />
+                    </Zoom>
+                </div>                
             </div>
             <div className="mt-3 text-sm lg:text-base">Jam Tangan Casio</div>
             <div className='text-xs my-2 text-neutral3'>Aksesoris</div>
-            <div className="text-xs lg:text-sm">Rp. 250.000</div>
+            <div className="text-xs lg:text-sm">Rp. {props.product.price}</div>
         </div>
         
         
